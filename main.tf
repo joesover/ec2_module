c007662a08fec61416705f6b83991ee1bb016149
Joseph Exaltacion <joseph.exaltacion@rackspace.co.uk>
2025-03-05 19:47:01 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+variable "instance_names" {
+  description = "List of instance names"
+  type        = list(string)
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "example" {
+  for_each = { for idx, name in var.instance_names : idx => name } # Convert list to map
+
+  ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = each.value
+  }
+}
+
+output "instance_ids" {
+  value = [for instance in aws_instance.example : instance.id]
+}

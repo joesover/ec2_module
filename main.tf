2c7ed64ad5cc4e79b38d46f7892cc7a92351a717
Joseph Exaltacion <joseph.exaltacion@rackspace.co.uk>
2025-03-05 22:19:36 +0000

    Update ami

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = "= 0.12.28" # Restricting Terraform version
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 variable "instance_names" {
   description = "List of instance names"
   type        = list(string)
@@ -15,7 +26,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   for_each = { for idx, name in var.instance_names : idx => name } # Convert list to map
 
-  ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID
+  ami           = "ami-00710ab5544b60cf7" # Replace with a valid AMI ID
   instance_type = "t2.micro"
 
   tags = {
